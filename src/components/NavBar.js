de571eb4cdb3523aedca188a170e9af2791f0cac
Saikou Gassama <gassamasaikou@yahoo.com>
2023-12-31 17:51:10 +0000

    Add: NavBar styles and NavBar.module.css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,36 +1,43 @@
 import React from 'react'
 import { Navbar, Container, Nav, NavDropdown} from "react-bootstrap";
 import logo from "../assets/Catch-up-logo.png"
+import styles from "../styles/NavBar.module.css";
+
 
 
 
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed='top' className="bg-body-tertiary">
+    <Navbar className={styles.NavBar}  expand="md" fixed='top'>
       <Container>
-        <Navbar.Brand > <img src={logo} alt="logo" height="50" /></Navbar.Brand>
-        <h4 >
+        <Navbar.Brand > <img src={logo}  alt="logo" height="50" /></Navbar.Brand>
+        <h4 className={`${styles.Loggo} mr-3 mt-1`} style={{ color: "white" }} >
           Catch-Up
         </h4>
+        <NavDropdown title="Categories" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Travel</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Opinion</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Style</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Health</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Science</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Cultuure</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Technology</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Politics</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Buisiness</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Design</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
             <Nav.Link ><i className="fa-solid fa-house" />Home</Nav.Link>
             <Nav.Link ><i className="fa-solid fa-arrow-right-to-bracket" />Sign in</Nav.Link>
             <Nav.Link ><i className="fa-solid fa-user-plus" />Sign up</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+           
           </Nav>
         </Navbar.Collapse>
       </Container>
