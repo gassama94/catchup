ee1afe0eabd8fa9f623724193c1d44100b0b34d9
Saikou Gassama <gassamasaikou@yahoo.com>
2024-01-01 19:04:23 +0000

    Feat: Add PostEditForm component for editing existing posts

--- src/pages/posts/PostEditForm.js
+++ src/pages/posts/PostEditForm.js
@@ -1,30 +1,25 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import Alert from "react-bootstrap/Alert";
-import Image from "react-bootstrap/Image";
-
-
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-
-
-import Asset from "../../components/Asset";
-
-import Upload from "../../assets/upload.png";
 
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
-
-import { useHistory } from "react-router";
+import { Alert, Image } from "react-bootstrap";
+import {
+  useHistory,
+  useParams,
+} from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 
-function PostCreateForm() {
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+function PostEditForm() {
   const [errors, setErrors] = useState({});
 
   const [postData, setPostData] = useState({
@@ -34,11 +29,16 @@ function PostCreateForm() {
     content: "",
     image: "",
   });
-  const { category, title, content, image, excerpt} = postData;
 
+  const { category, title, content, image, excerpt } = postData;
+
+  // References the image form.file element
   const imageInput = useRef(null);
 
+  // Used to redirect users to different pages
   const history = useHistory();
+  // Used to get the id parameter out of the url
+  const { id } = useParams();
 
   // Toolbar options module for React Quill editor
   var toolbarOptions = [
@@ -58,7 +58,36 @@ function PostCreateForm() {
     toolbar: toolbarOptions,
   };
 
-   // Function for React Quill onChange attribute
+  // Function for editing posts
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        // Retrieves specific post with it's id
+        const { data } = await axiosReq.get(`/posts/${id}/`);
+        const { category, title, excerpt, content, image, is_owner } = data;
+        // Checks if the user is the post owner, before updating
+        // the fields below
+        is_owner
+          ? setPostData({ category, title, excerpt, content, image })
+          : // If the user isn't the post owner, redirects to homepage
+            history.push("/");
+      } catch (err) {
+        // console.log(err);
+      }
+    };
+
+    handleMount();
+    // Only runs if history or id dependencies change
+  }, [history, id]);
+
+  const handleChange = (event) => {
+    setPostData({
+      ...postData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  // Function for React Quill onChange attribute
   // Matches value to content
   const handleChangeContent = (value) => {
     setPostData((prev) => {
@@ -69,15 +98,12 @@ function PostCreateForm() {
     });
   };
 
-  const handleChange = (event) => {
-    setPostData({
-      ...postData,
-      [event.target.name]: event.target.value,
-    });
-  };
-
+  // Function for image field upload
   const handleChangeImage = (event) => {
+    // First checks if there is a file
     if (event.target.files.length) {
+      // In case the user decides to change their image
+      // file after adding one, revoke is called
       URL.revokeObjectURL(image);
       setPostData({
         ...postData,
@@ -90,26 +116,39 @@ function PostCreateForm() {
     event.preventDefault();
     const formData = new FormData();
 
-
-    formData.append("title", title);
+    // Assign each type of data to the correct variable defined above
     formData.append("category", category);
+    formData.append("title", title);
     formData.append("excerpt", excerpt);
     formData.append("content", content);
-    formData.append("image", imageInput.current.files[0]);
 
+    // First checks if the image input element already has a file in it
+    if (imageInput?.current?.files[0]) {
+      // If it does, appends the image
+      formData.append("image", imageInput.current.files[0]);
+    }
+
+    // Need to refresh access token for sending an image file
     try {
-      const { data } = await axiosReq.post("/posts/", formData);
-      history.push(`/posts/${data.id}`);
-    } catch (err) {
-      console.log(err);
-      if (err.response?.status !== 401) {
-        setErrors(err.response?.data);
+      // put method to update existing post
+      // Passes post id into api update string so it
+      // knows which post to update
+      await axiosReq.put(`/posts/${id}/`, formData);
+      // Redirects to specific post after editing it
+      history.push(`/posts/${id}`);
+    } catch (error) {
+      // console.log(error);
+      // If the error status is NOT a 401 error
+      if (error.response?.status !== 401) {
+        setErrors(error.response?.data);
       }
     }
   };
 
   const textFields = (
     <div className="text-center">
+      {/* Add your form fields here */}
+
       <Form.Group>
         <Form.Label>Title</Form.Label>
         <Form.Control
@@ -119,13 +158,14 @@ function PostCreateForm() {
           onChange={handleChange}
         />
       </Form.Group>
+      {/* Displays errors */}
       {errors?.title?.map((message, idx) => (
         <Alert variant="warning" key={idx}>
           {message}
         </Alert>
       ))}
 
-<Form.Group>
+      <Form.Group>
         <Form.Label>Category</Form.Label>
         <Form.Control
           as="select"
@@ -169,31 +209,13 @@ function PostCreateForm() {
           {message}
         </Alert>
       ))}
-
       <Form.Group>
         <Form.Label>Content</Form.Label>
-        <Form.Control
-          as="textarea"
-          rows={6}
-          name="content"
-          value={content}
-          onChange={handleChange}
-        />
-      </Form.Group>
-      {errors?.content?.map((message, idx) => (
-        <Alert variant="warning" key={idx}>
-          {message}
-        </Alert>
-      ))}
-       <Form.Group>
-        <Form.Label>Content</Form.Label>
         <ReactQuill
-          modules={module}
-          className={styles.QlContainer}
           theme="snow"
+          modules={module}
           value={content}
           onChange={handleChangeContent}
-          placeholder={"A canvas for your thoughts..."}
         />
       </Form.Group>
       {errors?.content?.map((message, idx) => (
@@ -201,7 +223,6 @@ function PostCreateForm() {
           {message}
         </Alert>
       ))}
-
       <Button
         className={`${btnStyles.Button} ${btnStyles.Blue}`}
         onClick={() => history.goBack()}
@@ -209,45 +230,34 @@ function PostCreateForm() {
         cancel
       </Button>
       <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
-        create
+        save
       </Button>
     </div>
   );
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Row>
+      <Row className="d-flex justify-content-center">
         <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
           <Container
             className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
           >
             <Form.Group className="text-center">
-              {image ? (
-                <>
-                  <figure>
-                    <Image className={appStyles.Image} src={image} rounded />
-                  </figure>
-                  <div>
-                    <Form.Label
-                      className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
-                      htmlFor="image-upload"
-                    >
-                      Change the image
-                    </Form.Label>
-                  </div>
-                </>
-              ) : (
+              {/* Displays image preview if they have chosen one */}
+
+              <figure>
+                <Image className={appStyles.Image} src={image} rounded />
+              </figure>
+              <div>
                 <Form.Label
-                  className="d-flex justify-content-center"
+                  className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
                   htmlFor="image-upload"
                 >
-                  <Asset
-                    src={Upload}
-                    message="Click or tap to upload an image"
-                  />
+                  Change the image
                 </Form.Label>
-              )}
+              </div>
 
+              {/* Asterisk on accept is so that users can only upload images */}
               <Form.File
                 id="image-upload"
                 accept="image/*"
@@ -260,16 +270,12 @@ function PostCreateForm() {
                 {message}
               </Alert>
             ))}
-
-            <div className="d-md-none">{textFields}</div>
+            <div className="text-center">{textFields}</div>
           </Container>
-        </Col>
-        <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
-          <Container className={appStyles.Content}>{textFields}</Container>
         </Col>
       </Row>
     </Form>
   );
 }
 
-export default PostCreateForm;
+export default PostEditForm;
